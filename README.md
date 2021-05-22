144922f23cf77803c73a1b4302706ae6c36ac786
DustinLin <dustinylin@gmail.com>
2021-05-22 09:56:14 -0700

    submit commit

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 ## Check your understanding q's (FILL OUT)
 1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
+- 1
 
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+- We should not use a unit test here since there are actually many opertaions that go into "writing and sending a message to another user". We can break this task down further into operation like correctly capturing what the user wrote, and actually sending the message to another user.
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
+- We should use a unit test here to test the "max message length" feature. Since this is a single opertion that can't be broken down further.
 
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+- The test would run without showing me the browser UI.
 
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+```
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5501');
+    await page.waitForTimeout(500);
+    await expect(page).toClick('header > img');
+  });
+
+```
 
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,6 +1,11 @@
+
+// TBH IDK why my live-server is now at port 5501 instead of 5500 
+
+import { PageEmittedEvents } from "puppeteer";
+
 describe('Basic user flow for SPA ', () => {
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:5500');
+    await page.goto('http://127.0.0.1:5501');
     await page.waitForTimeout(500);
   });
 
@@ -29,13 +34,22 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
+    await expect(page).toClick('main :nth-child(1)');
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#entry1'); //toMatch from puppeteer docs
 
-  });
+  }, 10000);
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
+    //$ is query selector<, $eval does it and passes it to function after
+    // can do 'header > h1', but doing below will just grab first h1
+    const headerText = await page.$eval('h1', (header) => {
+      return header.innerHTML;
+    })
+    expect(headerText).toMatch('Entry 1')
 
-  });
+  }, 10000);
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
     /*
@@ -50,58 +64,173 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+     //get the entry
+     const entryPage = await page.$('entry-page');
+     //get the 'entry' value from it
+     const entryProp = await entryPage.getProperty('entry');
+     //convert that to json
+     const entryJson = await entryProp.jsonValue();
+     expect(entryJson.title).toMatch('You like jazz?');
+     expect(entryJson.date).toMatch('4/25/2021');
+     expect(entryJson.content).toMatch("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.");
+     expect(entryJson.image.src).toMatch('https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455');
+     expect(entryJson.image.alt).toMatch('bee with sunglasses');
 
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
+    const bodyClasses = await page.$eval('body', (body) => {
+      return body.classList;
+    });
+    expect(bodyClasses[0]).toMatch('single-entry');
 
-  });
+  }, 10000);
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
+    // can use 'header > img', but doing below will grab first img (which happens to be the setting)
+    await expect(page).toClick('img');
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#settings');
 
-  });
+  }, 10000);
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
 
-  });
+    const headerText = await page.$eval('h1', (header) => {
+      return header.innerHTML;
+    })
+    expect(headerText).toMatch('Settings');
+
+  }, 10000);
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
+    // if it has one class, can just use className
+    const bodyClass = await page.$eval('body', (body) => {
+      return body.className;
+    });
+    expect(bodyClass).toMatch('settings');
 
-  });
+  }, 10000);
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
+    await page.goBack();
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#entry1');
 
-  });
+  }, 10000);
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
+  it('Test11: Clicking the back button once should bring the user back to the home back', async() => {
+    await page.goBack();
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501');
+
+  }, 10000);
 
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
+  it('Test12: When the user if on the homepage, the header should be "Journal Entries" ', async () => {
+    const headerText = await page.$eval('h1', (header) => {
+      return header.innerHTML;
+    })
+    expect(headerText).toMatch('Journal Entries');
 
+  }, 10000);
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
+  it('Test13: On the home page the <body> element should not have any class attribute', async () => {
+    const bodyClass = await page.$eval('body', (body) => {
+      return body.className;
+    });
+    expect(bodyClass).toMatch('');
 
+  }, 10000);
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
+  it('Test14: Clicking the second <journal-entry>, new URL should contain /#entry2', async () => {
+    // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
+    await expect(page).toClick('main :nth-child(2)');
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#entry2'); //toMatch from puppeteer docs
 
+  }, 10000);
 
   // define and implement test15: Verify the title is current when clicking on the second entry
+  it('Test15: On second entry, title should be "Entry 2"', async () => {
+    // implement test8: Clicking on the settings icon should update the header to be “Settings”
 
+    const headerText = await page.$eval('h1', (header) => {
+      return header.innerHTML;
+    })
+    expect(headerText).toMatch('Entry 2');
+
+  }, 10000);
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  it('Test16: On second Entry page - checking <entry-page> contents', async () => {
+    /*
+    {
+      audio: null
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get."
+      date: "4/26/2021"
+      image: Object { src: "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg", alt: "forrest running" }
+      title: "Run, Forrest! Run!"
 
+    }
+      */
+    //get the entry
+    const entryPage = await page.$("entry-page");
+    //get the 'entry' value from it
+    const entryProp = await entryPage.getProperty("entry");
+    //convert that to json
+    const entryJson = await entryProp.jsonValue();
+    expect(entryJson.title).toMatch("Run, Forrest! Run!");
+    expect(entryJson.date).toMatch("4/26/2021");
+    expect(entryJson.content).toMatch(
+      "Mama always said life was like a box of chocolates. You never know what you're gonna get."
+    );
+    expect(entryJson.image.src).toMatch(
+      "https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg"
+    );
+    expect(entryJson.image.alt).toMatch("forrest running");
+    expect(entryJson.audio).toBeUndefined();
+  }, 10000);
 
   // create your own test 17
+  it('Test17: Clicking the back button once should bring the user back to the home back', async() => {
+    await page.goBack();
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501');
+
+  }, 10000);
 
   // create your own test 18
+  it('Test18: clikcing the 8th entry shows correct url ', async() => {
+    await expect(page).toClick('main :nth-child(8)');
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#entry8');
+
+  }, 10000);
 
   // create your own test 19
+  it('Test18: Clicking the settings icon, new URL should contain #settings', async () => {
+    // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
+    await expect(page).toClick('img');
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#settings');
+
+  }, 10000);
 
   // create your own test 20
-  
+  it('Test20: Clicking the back button once should bring the user back to entry 8', async() => {
+    await page.goBack();
+    const url = await page.evaluate(() => location.href);
+    expect(url).toMatch('http://127.0.0.1:5501/#entry8');
+
+  }, 10000);
 });
